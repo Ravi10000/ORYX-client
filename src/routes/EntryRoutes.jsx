197f4c56f5128c51ca0bc8651373862313ef656f
Nitesh Kumar <ndelhi66@gmail.com>
2023-12-29 12:11:07 +0530

    Bug fixes

--- src/routes/EntryRoutes.jsx
+++ src/routes/EntryRoutes.jsx
@@ -1,13 +1,13 @@
 import LoginPage from "../pages/Auth/LoginPage";
 import Register from "../pages/Auth/Register";
-import ResetPassword from "../pages/Auth/ResetPassword";
+import ResetPasswordPage from "../pages/Auth/ResetPasswordPage";
 
 const EntryRoutes = [
   { path: "register", element: <Register /> },
   { path: "login", element: <LoginPage /> },
   {
     path: "reset-password",
-    element: <ResetPassword />,
+    element: <ResetPasswordPage />,
     children: [],
   },
 ];
