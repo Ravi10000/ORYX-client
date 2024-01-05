c4ea7f275893fe937ac1bb1801002e7889e88bf4
Nitesh Kumar <ndelhi66@gmail.com>
2024-01-05 12:56:48 +0530

    Updated Document type

--- src/pages/myProperties/addProperty/tabs/Documents/documentTypeTableRow.jsx
+++ src/pages/myProperties/addProperty/tabs/Documents/documentTypeTableRow.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function TableRow() {
+export default function DocumentTypeTableRow({ documentType }) {
     return (
         <tr className='border-b-2 border-[#eef2f6]'>
-            <td className='p-3 text-center'>Document Name</td>
+            <td className='p-3 text-center'>{documentType.name}</td>
+
+            <td className={`p-3 text-center font-medium ${documentType.isRequired === 'Yes' ? 'text-primary' : 'text-red-500'}`}>{documentType.isRequired}</td>
 
             <td className='p-3 text-center flex gap-5 justify-center items-center'>
                 <span>Upload</span>
--- src/pages/myProperties/addProperty/tabs/Documents/index.jsx
+++ src/pages/myProperties/addProperty/tabs/Documents/index.jsx
@@ -1,25 +1,41 @@
-import React from 'react'
-import TableRow from './TableRow'
+import React, { useEffect, useState } from 'react'
+import DocumentTypeTableRow from './documentTypeTableRow'
+import { authApi } from '../../../../../api';
 
 export default function Documents({ setActiveTab }) {
+
+    const [documentTypes, setDocumentTypes] = useState();
+
+    async function getDocumentTypes() {
+        const { data: { data } } = await authApi.get('/document-type/read');
+        console.log(data);
+        setDocumentTypes(data);
+    }
+
+    useEffect(() => {
+        getDocumentTypes();
+    }, []);
+
     return (
         <div>
             <table className='w-full'>
                 <thead className='bg-[#f1f8ff]'>
                     <tr>
                         <th className='p-3 font-semibold text-center'>Rent Document</th>
+                        <th className='p-3 font-semibold text-center'>Required</th>
                         <th className='p-3 font-semibold text-center'>Upload / View</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    <TableRow />
-                    <TableRow />
-                    <TableRow />
-                    <TableRow />
-                    <TableRow />
-                    <TableRow />
-                    <TableRow />
+                    {documentTypes?.map((documentType) => (
+                        <DocumentTypeTableRow
+                            key={documentType?._id}
+                            getDocumentTypes={getDocumentTypes}
+                            // setModal={setModal}
+                            documentType={documentType}
+                        />
+                    ))}
                 </tbody>
             </table>
             <div className='mt-10 flex justify-end items-center'>
